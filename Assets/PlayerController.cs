24c3a167c28145205833cb5e5353799aa61c530b
AmuntVal <iamchuckliu@126.com>
2020-12-14 09:39:57 +0800

    AI

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Camera cam;
+
+    public NavMeshAgent agent;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            Physics.Raycast(ray, out hit);
+
+            //agent.SetDestination(hit.point);
+            
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
+    }
+}
